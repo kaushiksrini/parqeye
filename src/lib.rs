ca9925e0821cca3a426b024cf41565529f256775
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2025-09-05 09:19:34 -0400

    remove metadata file

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@ pub mod app;
 pub mod column_chunk;
 pub mod components;
 pub mod dictionary;
-pub mod metadata;
+pub mod file;
 pub mod schema;
 pub mod stats;
 pub mod ui;
 pub mod utils;
 pub mod visualize;
+pub mod tabs;
+
 
 pub use app::App;
+
