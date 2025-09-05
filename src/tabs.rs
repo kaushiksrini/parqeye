206e7ea7f9a6df07c1ee716189d3640c3d93e31c
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2025-09-05 15:28:58 -0400

    update tabs with prev function, add renderable

--- src/tabs.rs
+++ src/tabs.rs
@@ -2,14 +2,15 @@ use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
 use ratatui::text::Line;
 use ratatui::widgets::Widget;
-use ratatui::widgets::{Block, Tabs};
+use ratatui::widgets::Tabs;
+
+use crate::file::Renderable;
 
 pub enum TabType {
     Visualize,
     Metadata,
     Schema,
     RowGroups,
-    Visualize,
 }
 
 impl std::fmt::Display for TabType {
@@ -19,7 +20,6 @@ impl std::fmt::Display for TabType {
             TabType::Metadata => write!(f, "Metadata"),
             TabType::Schema => write!(f, "Schema"),
             TabType::RowGroups => write!(f, "Row Groups"),
-            TabType::Visualize => write!(f, "Visualize"),
         }
     }
 }
@@ -34,29 +34,41 @@ pub struct TabManager {
 impl TabManager {
     pub fn new() -> Self {
         Self {
-            tabs: vec![TabType::Schema, TabType::RowGroups, TabType::Visualize],
+            tabs: vec![TabType::Visualize, TabType::Metadata, TabType::Schema, TabType::RowGroups],
             active_tab: 0,
             title: "Tabs".to_string(),
         }
+    }
+
+    pub fn next(&mut self) {
+        self.active_tab = (self.active_tab + 1) % self.tabs.len();
+    }
+
+    pub fn prev(&mut self) {
+        if self.active_tab == 0 {
+            self.active_tab = self.tabs.len() - 1;
+        } else {
+            self.active_tab = (self.active_tab.saturating_sub(1)) % self.tabs.len();
+        }
+    }
+
+    pub fn active_tab(&self) -> &TabType {
+        &self.tabs[self.active_tab]
     }
 
     pub fn with_selected_tab(mut self, active_tab: usize) -> Self {
         self.active_tab = active_tab;
         self
     }
-
-    pub fn with_title(mut self, title: String) -> Self {
-        self.title = title;
-        self
-    }
 }
 
-impl Widget for TabManager {
-    fn render(self, area: Rect, buf: &mut Buffer) {
+impl Renderable for TabManager {
+    fn render_content(&self, area: Rect, buf: &mut Buffer) {
         let tab_titles: Vec<Line> = self.tabs.iter().map(|t| Line::from(t.to_string())).collect();
-        let tabs_widget = Tabs::new(tab_titles)
+        let tabs_widget: Tabs<'_> = Tabs::new(tab_titles)
             .select(self.active_tab)
-            .block(Block::bordered().title(self.title));
+            .padding(" ", " ")
+            .divider(" ");
 
         tabs_widget.render(area, buf);
     }
