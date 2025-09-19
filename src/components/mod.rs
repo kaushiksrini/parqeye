baa72f591ea5ec94e55465f36f316083826e5bf6
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2025-09-18 23:15:08 -0400

    update components mod file

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,11 @@
+pub mod chart;
+pub mod column_sizes;
+pub mod schema_table;
 pub mod schema_tree;
 pub mod scrollbar;
-pub mod schema_table;
 
+pub use chart::LineChart;
+pub use column_sizes::ColumnSizesButterflyChart;
+pub use schema_table::FileSchemaTable;
 pub use schema_tree::SchemaTreeComponent;
 pub use scrollbar::ScrollbarComponent;
-pub use schema_table::FileSchemaTable;
