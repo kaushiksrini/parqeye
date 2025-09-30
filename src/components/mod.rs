ac85ddb4827efb0e5e6938a9b58dc890aaf074c5
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2025-09-29 23:37:42 -0400

    remove column sizes chart for space

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,4 @@
 pub mod chart;
-pub mod column_sizes;
 pub mod data_table;
 pub mod row_group_metadata;
 pub mod schema_table;
@@ -7,7 +6,6 @@ pub mod schema_tree;
 pub mod scrollbar;
 
 pub use chart::LineChart;
-pub use column_sizes::ColumnSizesButterflyChart;
 pub use data_table::DataTable;
 pub use row_group_metadata::RowGroupMetadata;
 pub use schema_table::FileSchemaTable;
