e5f038cc8682cc601de5ea588df681f55f2e1406
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2025-10-17 00:23:48 -0400

    reorganize components

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,15 +1,12 @@
 pub mod data_table;
-pub mod row_group_metadata;
-pub mod row_group_progress;
-pub mod row_group_schema_md;
-pub mod schema_table;
-pub mod schema_tree;
+pub mod row_group;
+pub mod schema;
 pub mod scrollbar;
 
 pub use data_table::DataTable;
-pub use row_group_metadata::RowGroupMetadata;
-pub use row_group_progress::RowGroupProgressBar;
-pub use row_group_schema_md::RowGroupColumnMetadataComponent;
-pub use schema_table::FileSchemaTable;
-pub use schema_tree::SchemaTreeComponent;
+pub use row_group::RowGroupMetadata;
+pub use row_group::RowGroupProgressBar;
+pub use row_group::RowGroupColumnMetadataComponent;
+pub use schema::FileSchemaTable;
+pub use schema::SchemaTreeComponent;
 pub use scrollbar::ScrollbarComponent;
---
+++ src/components/row_group/mod.rs
@@ -0,0 +1,7 @@
+pub mod metadata;
+pub mod progress_bar;
+pub mod schema_md;
+
+pub use metadata::RowGroupMetadata;
+pub use progress_bar::RowGroupProgressBar;
+pub use schema_md::RowGroupColumnMetadataComponent;
